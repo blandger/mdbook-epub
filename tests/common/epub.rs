59d3f89b8c7e0fc3f942e5aac487b3a5940c48fd
blandger <blandger@gmail.com>
2025-12-01 09:47:10 +0200

    minor fixes after rebase from master

--- tests/common/epub.rs
+++ tests/common/epub.rs
@@ -51,27 +51,6 @@ pub fn generate_epub_preserve_temp_folder(
     }
 }
 
-#[allow(dead_code)]
-pub fn generate_epub_preserve_temp_folder(
-    epub_book_name: &str,
-) -> Result<(EpubDoc<BufReader<File>>, PathBuf), Error> {
-    debug!("generate_epub: {:?}...", epub_book_name);
-    let (ctx, _md, temp) = create_dummy_book_preserve_temp_folder(epub_book_name).unwrap();
-    debug!("temp dir = {:?}", &temp);
-    debug!("Before start generate...");
-    mdbook_epub::generate(&ctx)?;
-    let output_file = mdbook_epub::output_filename(&temp, &ctx.config)?;
-    debug!("output_file = {:?}", &output_file.display());
-
-    match EpubDoc::new(&output_file) {
-        Ok(epub) => Ok((epub, output_file)),
-        Err(err) => {
-            error!("dummy book creation error = {:?}", err);
-            Err(Error::EpubDocCreate(output_file.display().to_string()))
-        }
-    }
-}
-
 /// Use `MDBook::load()` to load the dummy book into memory, then set up the
 /// `RenderContext` for use the EPUB generator.
 pub fn create_dummy_book(name: &str) -> Result<(RenderContext, MDBook, TempDir), Error> {
@@ -94,32 +73,6 @@ pub fn create_dummy_book(name: &str) -> Result<(RenderContext, MDBook, TempDir),
     );
 
     Ok((ctx, book, temp))
-}
-
-pub fn create_dummy_book_preserve_temp_folder(
-    name: &str,
-) -> Result<(RenderContext, MDBook, PathBuf), Error> {
-    debug!("create_{:?}...", name);
-    let temp = TempDir::with_prefix_in("mdbook-epub", ".")?;
-    let temp_path: PathBuf = temp.into_path();
-    debug!("Temporary directory preserved at: {:?}", temp_path);
-
-    let dummy_book = Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("tests")
-        .join(name);
-    debug!("{:?} = {:?}", name, &dummy_book.display().to_string());
-
-    let md = MDBook::load(dummy_book);
-
-    let book = md.expect(&format!("{:?} MDBook is not loaded", name));
-    let ctx = RenderContext::new(
-        book.root.clone(),
-        book.book.clone(),
-        book.config.clone(),
-        temp_path.to_path_buf(),
-    );
-
-    Ok((ctx, book, temp_path))
 }
 
 pub fn create_dummy_book_preserve_temp_folder(
@@ -202,24 +155,6 @@ pub fn output_epub_is_valid(epub_book_name: &str) {
     mdbook_epub::generate(&ctx).unwrap();
 
     let output_file = mdbook_epub::output_filename(temp.path(), &ctx.config);
-    assert!(output_file.is_ok());
-    let output_file = output_file.unwrap();
-    let got = EpubDoc::new(&output_file);
-
-    assert!(got.is_ok());
-
-    // also try to run epubcheck, if it's available
-    epub_check(&output_file).unwrap();
-}
-
-#[allow(dead_code)]
-pub fn output_epub_is_valid_preserve_temp_folder(epub_book_name: &str) {
-    init_logging();
-    debug!("output_epub_is_valid...");
-    let (ctx, _md, temp) = create_dummy_book_preserve_temp_folder(epub_book_name).unwrap();
-    mdbook_epub::generate(&ctx).unwrap();
-
-    let output_file = mdbook_epub::output_filename(&temp, &ctx.config);
     assert!(output_file.is_ok());
     let output_file = output_file.unwrap();
     let got = EpubDoc::new(&output_file);
